6edf28d76ddac142537cb62b10ec13e27d72a0d8
Piotr Drabik <pdrabik223@gmail.com>
2026-06-21 19:59:29 +0200

    local serwer setup

--- i2c_communication/main_master.py
+++ i2c_communication/main_master.py
@@ -2,16 +2,8 @@ import time
 
 import machine
 
-led = machine.Pin("LED", machine.Pin.OUT).value(1)
-
-# i2c = machine.I2C(0, scl=machine.Pin(17), sda=machine.Pin(16))
-
-# devices = i2c.scan()
+status_led = machine.Pin("LED", machine.Pin.OUT).value(1)
 
 if __name__ == "__main__":
     while 1 < 2:
         time.sleep(1)
-
-    # if devices:
-    #     for d in devices:
-    #         print(hex(d))
--- main.py
+++ main.py
@@ -1,65 +1,22 @@
-import time
-from machine import Pin
-import time
-from pi_pico_neopixel_tools.led_strip import LedStrip, Color
+import socket
+from pi_pico_w_server_tools.app import App, compose_response, load_html
 
-leds = LedStrip(71 + 8, 6)
-leds.fill(color=Color.black())
-
-cell_leds = [73, 74, 78, 77]
-leds.set_pixel(73, Color.warm_white())
-leds.set_pixel(78, Color.warm_white())
-
-hal_prw_line = [
-    Pin(2, Pin.OUT, 0),
-    Pin(3, Pin.OUT, 0),
-]
-
-hal_data_line = [
-    Pin(5, Pin.IN, Pin.PULL_UP),
-    Pin(4, Pin.IN, Pin.PULL_UP),
-]
+app = App(hostname="board.local")
 
 
-hal_data = [[1 for _ in range(len(hal_data_line))] for _ in range(len(hal_prw_line))]
+def home_page(cl: socket.socket, parameters: dict):
+    cl.sendall(
+        compose_response(
+            response=load_html("static/index.html"),
+            content_type="text/html; charset=utf-8",
+        )
+    )
 
-new_hal_data = [
-    [1 for _ in range(len(hal_data_line))] for _ in range(len(hal_prw_line))
-]
 
-led = Pin("LED", Pin.OUT)
-led.value(1)
+if __name__ == "__main__":
+    app.register_endpoint("/v1", home_page)
 
-time.sleep(5)
-iterator = 0
-try:
-    while True:
-        for pwr in range(len(hal_prw_line)):
-            hal_prw_line[pwr].value(1)
-            time.sleep(0.01)
-
-            for data in range(len(hal_data_line)):
-                value = hal_data_line[data].value()
-                new_hal_data[pwr][data] = value
-                # print("pwr:", pwr, "data:", data, "val", value)
-
-            hal_prw_line[pwr].value(0)
-
-        changed_list = []
-        for pwr in range(len(hal_prw_line)):
-            for data in range(len(hal_data_line)):
-
-                if hal_data[pwr][data] != new_hal_data[pwr][data]:
-                    changed_list.append([pwr, data])
-                    hal_data[pwr][data] = new_hal_data[pwr][data]
-
-        for change in changed_list:
-            leds.set_pixel(
-                cell_leds[change[0] * 2 + change[1]],
-                Color.colors()[iterator],
-            )
-            iterator += 1 
-            iterator %= len(Color.colors())
-
-except KeyboardInterrupt:
-    print("Program stopped.")
+    try:
+        app.main_loop()
+    except (KeyboardInterrupt, Exception) as ex:
+        print(f"Server error type: {type(ex)}\tmessage: {ex}\texiting")
