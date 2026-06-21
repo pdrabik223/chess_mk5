59383e4753c689d734e912744da39572efb5ee84
Piotr Drabik <pdrabik223@gmail.com>
2026-06-21 09:18:42 +0200

    led test

--- i2c_communication/main_master.py
+++ i2c_communication/main_master.py
@@ -1,10 +1,17 @@
+import time
+
 import machine
 
-i2c = machine.I2C(0, scl=machine.Pin(17), sda=machine.Pin(16))
+led = machine.Pin("LED", machine.Pin.OUT).value(1)
 
-devices = i2c.scan()
+# i2c = machine.I2C(0, scl=machine.Pin(17), sda=machine.Pin(16))
+
+# devices = i2c.scan()
 
 if __name__ == "__main__":
-    if devices:
-        for d in devices:
-            print(hex(d))
+    while 1 < 2:
+        time.sleep(1)
+
+    # if devices:
+    #     for d in devices:
+    #         print(hex(d))
